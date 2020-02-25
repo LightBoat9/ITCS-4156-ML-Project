45203a9f4199916d7776b57e9f2156b8995e6b67
Luke <lsloop4@uncc.edu>
2020-02-25 18:47:06 -0500

    Most Actions Implemented

--- main_loop.py
+++ main_loop.py
@@ -9,13 +9,15 @@ CELL_SIZE = np.array([16, 16])
 
 clear_color = [115, 209, 94, 255]
 screen = None
-objects = []
+objects = {}
 paused = False
 steps = 0
 
 key_items = {
+    "bot": None,
     "hoe": None,
     "seeds": None,
+    "water": None,
 }
 
 class GridObject(object):
@@ -46,9 +48,10 @@ class Bot(GridObject, pygame.sprite.Sprite):
     PICKUP_HOE = 4
     PICKUP_SEEDS = 5
     PICKUP_WATER = 6
-    PLANT_SEED = 7
-    WATER_SEED = 8
-    DROP_ITEM = 9
+    TIL_GROUND = 7
+    PLANT_SEED = 8
+    WATER_SEED = 9
+    DROP_ITEM = 10
 
     def __init__(self) -> None:
         GridObject.__init__(self)
@@ -81,9 +84,28 @@ class Bot(GridObject, pygame.sprite.Sprite):
         elif action == Bot.PICKUP_SEEDS:
             self.holding = key_items["seeds"]
             remove_object(key_items["seeds"])
+        elif action == Bot.PICKUP_WATER:
+            self.holding = key_items["water"]
+            remove_object(key_items["water"])
+        elif action == Bot.TIL_GROUND:
+            plant = Plant()
+            plant.grid_position = self.grid_position
+            add_object(plant, self.grid_position)
+        elif action == Bot.PLANT_SEED:
+            ground_objects = get_objects_at(self.grid_position)
+            for o in ground_objects:
+                if isinstance(o, Plant):
+                    o.stage = Plant.STAGE_PLANTED
+                    break
+        elif action == Bot.WATER_SEED:
+            ground_objects = get_objects_at(self.grid_position)
+            for o in ground_objects:
+                if isinstance(o, Plant):
+                    o.stage = Plant.STAGE_GROWN
+                    break
         elif action == Bot.DROP_ITEM:
             self.holding.grid_position = self.grid_position
-            add_object(self.holding)
+            add_object(self.holding, self.holding.grid_position)
             self.holding = None
 
     def get_available_actions(self) -> int:
@@ -105,19 +127,39 @@ class Bot(GridObject, pygame.sprite.Sprite):
             Bot.MOVE_UP: [0, -1], 
             Bot.MOVE_DOWN: [0, 1]
         }
+
+        ground_objects = get_objects_at(self.grid_position)
+        
         for k, v in dirs.items():
             pos = self.grid_position + np.array(v)
             if is_inside_grid(pos) and not is_collision_at(pos):
                 actions[k] = True
 
         if self.holding == None:
-            for obj in get_objects_at(self.grid_position):
+            for obj in ground_objects:
                 if isinstance(obj, Hoe):
                     actions[Bot.PICKUP_HOE] = True
                 elif isinstance(obj, Seeds):
                     actions[Bot.PICKUP_SEEDS] = True
+                elif isinstance(obj, Water):
+                    actions[Bot.PICKUP_WATER] = True
         else:
             actions[Bot.DROP_ITEM] = True
+
+        plant_state = -1
+        for o in ground_objects:
+            if isinstance(o, Plant):
+                plant_state = o.stage
+                break
+
+        if plant_state == -1:
+            if isinstance(self.holding, Hoe):
+                actions[Bot.TIL_GROUND] = True
+        else:
+            if plant_state == Plant.STAGE_PLANTED and isinstance(self.holding, Water):
+                actions[Bot.WATER_SEED] = True
+            elif plant_state == Plant.STAGE_TILLED and isinstance(self.holding, Seeds):
+                actions[Bot.PLANT_SEED] = True
         
         return actions
 
@@ -138,6 +180,37 @@ class Seeds(GridObject, pygame.sprite.Sprite):
         self.image = pygame.image.load("seeds.png")
         self.grid_position = np.array([GRID_SIZE[0]-1, 4])
 
+class Water(GridObject, pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        GridObject.__init__(self)
+        self.image = pygame.image.load("water.png")
+        self.grid_position = np.array([8, 0])
+
+class Plant(GridObject, pygame.sprite.Sprite):
+    STAGE_TILLED = 0
+    STAGE_PLANTED = 1
+    STAGE_GROWN = 2
+
+    stage_surfaces = {
+        STAGE_TILLED: pygame.image.load("tilled.png"),
+        STAGE_PLANTED: pygame.image.load("planted.png"),
+        STAGE_GROWN: pygame.image.load("grown.png")
+    }
+
+    def __init__(self) -> None:
+        GridObject.__init__(self)
+        self._stage = 0
+        self.stage = Plant.STAGE_TILLED
+
+    @property
+    def stage(self) -> int:
+        return self._stage
+
+    @stage.setter
+    def stage(self, to: int) -> None:
+        self._stage = to
+        self.image = self.stage_surfaces[to]
+
 def draw_surface(surface: pygame.Surface, position: Iterable) -> None:
     screen.blit(surface, position)
 
@@ -145,11 +218,7 @@ def is_inside_grid(position: Iterable) -> bool:
     return (position[0] >= 0 and position[1] >= 0 and position[0] < GRID_SIZE[0] and position[1] < GRID_SIZE[1])
 
 def get_objects_at(position: Iterable) -> Iterable:
-    objs = []
-    for o in objects:
-        if np.array_equal(o.grid_position, position):
-            objs.append(o)
-    return objs
+    return objects[(position[0], position[1])]
 
 def is_collision_at(position: Iterable) -> bool:
     for obj in get_objects_at(position):
@@ -157,17 +226,27 @@ def is_collision_at(position: Iterable) -> bool:
     
     return False
 
-def add_object(obj: GridObject) -> None:
-    objects.append(obj)
+def move_object(obj: GridObject, position: Iterable) -> None:
+    remove_object(obj)
+    add_object(obj, position)
+
+def add_object(obj: GridObject, position: Iterable) -> None:
+    obj.grid_position = position
+    objects[(position[0], position[1])].append(obj)
 
 def remove_object(obj: GridObject) -> None:
-    if obj in objects:
-        objects.remove(obj)
+    position = obj.grid_position
+    if obj in objects[(position[0], position[1])]:
+        objects[(position[0], position[1])].remove(obj)
 
 def main() -> None:
     global screen
     global steps
     global key_items
+
+    for y in range(GRID_SIZE[1]):
+        for x in range(GRID_SIZE[0]):
+            objects[(x, y)] = []
 
     pygame.init()
     icon = pygame.image.load("icon.png")
@@ -176,11 +255,14 @@ def main() -> None:
 
     screen = pygame.display.set_mode((GRID_SIZE[0] * 16, GRID_SIZE[1] * 16), pygame.SCALED | pygame.RESIZABLE)
     running = True
-    add_object(Bot())
+    key_items["bot"] = Bot()
+    add_object(key_items["bot"], [8, 4])
     key_items["hoe"] = Hoe()
-    add_object(key_items["hoe"])
+    add_object(key_items["hoe"], [0, 4])
     key_items["seeds"] = Seeds()
-    add_object(key_items["seeds"])
+    add_object(key_items["seeds"], [15, 4])
+    key_items["water"] = Water()
+    add_object(key_items["water"], [8, 0])
 
     frame_ticks_last = pygame.time.get_ticks()
 
@@ -189,24 +271,28 @@ def main() -> None:
         delta_time = (frame_ticks - frame_ticks_last) / 1000.0
         frame_ticks_last = frame_ticks
 
-        if not paused:
-            for obj in objects:
-                obj._update(delta_time)
+        for i in range(1000):
+            if not paused:
+                for key in objects.keys():
+                    for obj in objects[key]:
+                        obj._update(delta_time)
+            
+            steps += 1
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                print(steps)
 
-            for obj in objects:
-                obj._input(event)
+            for key in objects.keys():
+                for obj in objects[key]:
+                    obj._input(event)
             
         screen.fill(clear_color)
-        for obj in objects:
-            obj._draw()
+        for key in objects.keys():
+            for obj in objects[key]:
+                obj._draw()
         pygame.display.update()
-        
-        steps += 1
-        #time.sleep(0.01)
 
 if __name__ == "__main__":
     main()
